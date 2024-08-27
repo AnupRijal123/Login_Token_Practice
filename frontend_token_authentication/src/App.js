8233b3df8708e95c82c5aef3ba4477b91b0a7608
Anup <anuprijal321@gmail.com>
2024-08-27 14:28:38 +0545

    implemented login funcationality using localStorage to store token

--- frontend_token_authentication/src/App.js
+++ frontend_token_authentication/src/App.js
@@ -1,15 +1,43 @@
 import './styles/App.css';
-import RegisterComponent from './components/RegisterComponent.js';
+import Homepage from './pages/Homepage.js';
+import Loginpage from './pages/Loginpage.js';
+import Registerpage from './pages/Registerpage.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="main-container">
-      <h2 className="title-text">Login Token Practice</h2>
+  const [user, setUser] = useState(false);
 
-      <div className="content-container">
-        <RegisterComponent />
+  //checking token in localStorage again and again to see token present or not 
+  setInterval(() => {
+    let token = localStorage.getItem('token');
+    if (token !== null) {
+      setUser(true);
+    } else {
+      setUser(false);
+    }
+  }, 1000);
+
+  return (
+    <BrowserRouter>
+      <div className="main-container">
+        <Routes>
+          {
+            user === false ?
+              (<Route path="/" element={<Loginpage />} />) :
+              (<Route path="/" element={<Homepage />} />)
+          }
+          {
+            user === false &&
+            <Route path="/register" element={<Registerpage />} />
+
+          }
+
+        </Routes>
+
       </div>
-    </div>
+
+    </BrowserRouter>
   );
 }
 
